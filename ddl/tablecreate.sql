77213d1cf9126efac325ebbd913b8f2ab338b0d2
Prakash71322 <srikrishnaprakashs@hexaware.com>
2022-12-29 17:37:13 +0530

    Initial Commit

---
+++ ddl/tablecreate.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS public.sample_table
+(
+    id bigint NOT NULL ,
+    name character varying(128) NOT NULL,
+    email character varying(254) 
+)
---
+++ dml/recordinsert.sql
@@ -0,0 +1,9 @@
+INSERT INTO TABLE public.sample_table VALUES (1,'TestData1','test1@tensai.com')
+INSERT INTO TABLE public.sample_table VALUES (2,'TestData2','test2@tensai.com')
+INSERT INTO TABLE public.sample_table VALUES (3,'TestData3','test3@tensai.com')
+INSERT INTO TABLE public.sample_table VALUES (4,'TestData4','test4@tensai.com')
+INSERT INTO TABLE public.sample_table VALUES (5,'TestData5','test5@tensai.com')
+INSERT INTO TABLE public.sample_table VALUES (6,'TestData6','test6@tensai.com')
+INSERT INTO TABLE public.sample_table VALUES (7,'TestData7','test7@tensai.com')
+INSERT INTO TABLE public.sample_table VALUES (8,'TestData8','test8@tensai.com')
+COMMIT;
